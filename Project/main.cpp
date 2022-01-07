cfbe708aefaf8e007edecb4e593546e5bc4bf55a
surejjaa <surejja.alibegovic.20@size.ba>
2022-01-07 12:28:45 +0100

    sort1

--- Project/main.cpp
+++ Project/main.cpp
@@ -13,6 +13,19 @@ int pocetniMenu()
 	{
 		std::cout << "Izbor: ";
 		std::cin >> izbor;
+		switch (izbor)
+		{
+		case 1:
+			a.dodajAdmina();
+			break;
+		case 2:
+			a.prijavaAdmina();
+			break;
+		case 3:
+			std::cout << "Hvala na prijavi!";
+			break;
+		}
+		system("pause");
 	} while (izbor < 1 || izbor>3);
 	std::cin.ignore();
 	return izbor;
@@ -32,6 +45,29 @@ int adminMenu()
 	{
 		std::cout << "Izbor: ";
 		std::cin >> izbor;
+		switch (izbor)
+		{
+		case 1:
+			dest.
+			break;
+		case 2:
+			o.smjestiPacijenta();
+			break;
+		case 3:
+			o.rezervisiKrevet();
+			break;
+		case 4:
+			std::cout << o;
+			std::cout << "Unesite broj kreveta za odjavu: ";
+			std::cin >> *n;
+			std::cin.ignore();
+			o.oslobodiKrevet(*n - 1);
+			break;
+		case 5:
+			std::cout << "Hvala i dovidjenja!";
+			break;
+		}
+		system("pause");
 	} while (izbor < 1 || izbor>5);
 	std::cin.ignore();
 	return izbor;
